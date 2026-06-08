04b7d34a3a49c9e01dc32daaaa8ceecb14e8a918
LeleHermoso <LETICIA.HERMOSO@ESCOLA.PR.GOV.BR>
2026-06-08 09:18:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btnMensagem").addEventListener("click", function() {
+    alert("A sustentabilidade começa com pequenas atitudes que fazem grande diferença para o planeta! 🌱");
+});
